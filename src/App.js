4d386aa1c96009543dcc27fa819e31164760883e
rakib <mdrakibhasan240@gmail.com>
2021-12-11 11:56:29 +0600

    FirstCommit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
-import Menu from './components/Menu';
+import Home from './components/page/Home';
+import About from './components/page/About';
+import Services from './components/page/Services';
+import Contact from './components/page/Contact';
+import Navbar from './components/Navbar';
+
+import Error from './components/page/Error';
+
+
 
 function App() {
   return (
-    <div className="App">
-		<Menu />
-    </div>
+		<Router>
+			<Navbar />
+			<Switch>
+			<Route exact path="/"  component={Home} />
+			<Route exact path="/about"  component={About} />
+			<Route exact path="/service"  component={Services} />
+			<Route exact path="/contact"  component={Contact} />
+
+
+			<Route component={Error} />
+			</Switch>
+      </Router>
+
   );
 }
 
